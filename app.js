df1733aee2f6d5ce97e447629971821b4aaf8113
joeyherederopal-design <joeyherederopal@gmail.com>
2026-02-07 08:17:18 +0800

    Update app.js

--- app.js
+++ app.js
@@ -352,4 +352,4 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('sw.js')
         .then(reg => console.log('Service Worker registered'))
         .catch(err => console.log('Service Worker registration failed'));
-}Update Google Apps Script URL
+} Update Google Apps Script URL
